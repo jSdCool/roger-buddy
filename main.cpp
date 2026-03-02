852a642c6aaa42906e95dac9988d05fc95a93205
jSdCool <jSdCoolgaming@gmail.com>
2026-03-02 16:25:47 -0500

    swimming animation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "raylib-cpp.hpp"
 #include <iostream>
+#include <vector>
 #include "mousePosition.hpp"
 
 #if defined(PLATFORM_WEB)
@@ -8,11 +9,15 @@
 
 using namespace std;
 
-int screenWidth = 300;
-int screenHeight = 300;
+int screenWidth = 250;
+int screenHeight = 250;
 
 raylib::Vector2 internalWindowPos;
 raylib::Texture2D roger1;
+int frame = 0;
+int counter =0;
+vector<raylib::Texture2D> rogerSwim;
+constexpr float SWIM_ANGLE_OFFSET = DEG2RAD * 30;
 
 Vector2 myVecToRayVec(myVector v) {
     return {v.x,v.y};
@@ -23,10 +28,15 @@ void UpdateDrawFrame(raylib::Window &window);
 int main() {
 
     //transparent background and no title bar / edge shaddow
-    unsigned int windowFlags = FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED;
+    unsigned int windowFlags = FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_TOPMOST;
     raylib::Window window(screenWidth, screenHeight, "Roger Buddy", windowFlags);
 
     roger1 = raylib::Texture2D("data/r1.png");
+
+    rogerSwim.reserve(10);
+    for (int i=0;i<10;i++) {
+        rogerSwim.emplace_back("data/rc"+to_string(i+1)+".png");
+    }
 
     window.SetIcon(roger1);
 
@@ -60,8 +70,9 @@ void UpdateDrawFrame(raylib::Window &window) {
     raylib::Vector2 targetPos = mousePos - (winSize/2.0f);
 
     float dist = targetPos.Distance(internalWindowPos);
+    float direction = atan2(targetPos.y-internalWindowPos.y,targetPos.x-internalWindowPos.x);
     if (dist > 40 ) {
-        float direction = atan2(targetPos.y-internalWindowPos.y,targetPos.x-internalWindowPos.x);
+
 
         raylib::Vector2 travel = {cos(direction),sin(direction)};
         travel *= windowVelocity;
@@ -70,6 +81,18 @@ void UpdateDrawFrame(raylib::Window &window) {
 
         window.SetPosition(newPos);//this uses the raw int of the pos so it can be weird
         internalWindowPos = newPos;
+    }
+
+    float swimAngle = direction - SWIM_ANGLE_OFFSET;
+    swimAngle *= RAD2DEG;
+
+    counter++;
+    if (counter == 9) {
+        counter = 0;
+        frame++;
+        if (frame == 10) {
+            frame = 0;
+        }
     }
 
     // Draw
@@ -81,7 +104,16 @@ void UpdateDrawFrame(raylib::Window &window) {
 
 
     DrawRectangle(0, 0, screenWidth, 15, WHITE);
-    roger1.Draw(0,40);
+
+    //draw the swiming animation
+    raylib::Rectangle dst {0.0f,0.0f,200.0f,200.0f};
+    if (frame == 9) {
+        dst.x = -2;
+        dst.y = 9;
+    }
+    dst.x+=125;
+    dst.y+=125;
+    rogerSwim[frame].Draw(Rectangle{0.0f,0.0f,static_cast<float>(rogerSwim[frame].width),static_cast<float>(rogerSwim[frame].height)},dst,{100,100},swimAngle);
 
 
     EndDrawing();
